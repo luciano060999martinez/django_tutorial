9bfa2523047316f729519e89f6fad57f4b31a304
luciano060999martinez <luciano.martinez060999@gmail.com>
2022-09-26 22:57:19 -0400

    render se usa

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /polls/
+    path('/polls/', views.index, name='index'),
+    # ex: /polls/5/
+    path('/polls/5/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('/polls/5/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('/polls/5/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hola mundo desde views!!!")
+from .models import Question
+from django.template import loader
 
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+    
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
@@ -13,3 +20,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
